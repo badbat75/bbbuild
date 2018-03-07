86ccc3adfc5eaad9b6c18b6b6af92e680ace95bf
Emiliano De Simoni <emiliano.desimoni@outlook.com>
2018-03-07 14:38:17 +0100

    cosmetic change

--- runinchroot_opt.sh
+++ runinchroot_opt.sh
@@ -361,16 +361,16 @@ MPCPID=$!
 mpc enable only 1
 kill $MPCPID
 
-echo //////////////////////////////////////////////////////////////
-echo 
-echo  STEP 10 - Optionally squash /var/www
-echo 
-echo //////////////////////////////////////////////////////////////
-
-echo NOTE: This is optional but highly recommended for performance/reliability
-
 if [ $ENABLE_SQUASHFS -eq 1 ]
 then
+    echo //////////////////////////////////////////////////////////////
+    echo 
+    echo  STEP 10 - Optionally squash /var/www
+    echo 
+    echo //////////////////////////////////////////////////////////////
+
+    echo NOTE: This is optional but highly recommended for performance/reliability
+
     cat <<EOF | sudo -i
 echo "/var/local/moode.sqsh   /var/www        squashfs        ro,defaults     0       0" >> /etc/fstab
 logout
